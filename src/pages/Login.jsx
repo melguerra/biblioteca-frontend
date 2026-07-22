cffa33f0d9a1a21cdce8b899d12ad4657d82c012
melguerra <melanigisel.guerrajorge@alumnos.uai.edu.ar>
2026-07-22 10:29:58 -0300

    Se conecta el login del frontend con el backend

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,19 +1,42 @@
 import { useState } from "react";
 
 function Login() {
-  const [usuario, setUsuario] = useState(""); //aca se guarda lo que el usuario escribe en el campo usuario //
-  const [password, setPassword] = useState("");
+const [email, setEmail] = useState("");
+const [password, setPassword] = useState("");
 
-const manejarLogin = (e) => {
+const manejarLogin = async (e) => {
   e.preventDefault();
 
-  if (usuario === "" || password === "") {
+  if (email === "" || password === "") {
     alert("Complete todos los campos.");
     return;
   }
 
-  alert("Más adelante este botón iniciará sesión.");
-};
+try {
+  const respuesta = await fetch("http://localhost:3000/api/usuarios/login", { //el fetch hace una peticion al backend para loguear al usuario, le envia el email y la contraseña que el usuario ingreso en el formulario
+    method: "POST", //el post indica que estamos enviando datos
+    headers: {  //le dice al servidor que los datos estan en formato json
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      email,
+      password,
+    }),
+  });
+
+  const datos = await respuesta.json(); //aca lee la rta que envia el backend
+
+  if (!respuesta.ok) {
+    alert(datos.mensaje);
+    return;
+  }
+
+  alert(datos.mensaje);  //aca se mostrara si el backend responde con errores
+
+} catch (error) {
+  console.log(error);
+  alert("Error al conectar con el servidor.");
+}};
 
   return (
     <div className="home">
@@ -23,12 +46,12 @@ const manejarLogin = (e) => {
       <form onSubmit={manejarLogin}>
 
         <div>
-          <label>Usuario</label>
+          <label>Email</label>
           <br />
           <input
             type="text"
-            value={usuario}
-            onChange={(e) => setUsuario(e.target.value)}
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
         </div>
 
