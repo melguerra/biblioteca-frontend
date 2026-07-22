787ae79ffd85a17b8fae6cc097cf816a4b75d0f1
melguerra <melanigisel.guerrajorge@alumnos.uai.edu.ar>
2026-07-22 17:29:30 -0300

    Se implementa logout y protección de rutas

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,28 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function Navbar() {
+
+const navigate = useNavigate();
+
+const cerrarSesion = () => {
+  localStorage.removeItem("token"); //aca borra el token
+  localStorage.removeItem("usuario"); //borra los datos del usuario
+
+  navigate("/");  //manda automaticamente al usuario al home
+};
   return (
     <nav className="navbar">
       <h2>📚 Biblioteca</h2>
 
       <div className="nav-links">
-        <Link to="/">Inicio</Link>
-        <Link to="/login">Login</Link>
-        <Link to="/registro">Registrarse</Link>
-      </div>
+  <Link to="/">Inicio</Link>
+  <Link to="/login">Login</Link>
+  <Link to="/registro">Registrarse</Link>
+
+  <button onClick={cerrarSesion}>
+    Cerrar sesión
+  </button>
+</div>
     </nav>
   );
 }
